963baade2f650d021d27f31cc4be4e74981066a8
Antaresque <olekfer@yahoo.pl>
2021-04-24 20:38:29 +0200

    docker-compose changes

--- services/data_service/src/routes/index.js
+++ services/data_service/src/routes/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 
-import getItems from './item/getItems';
-import addItem from './item/addItem';
-import updateItem from './item/updateItem';
-import deleteItem from './item/deleteItem';
+const getItems = require('./item/getItems');
+const addItem = require('./item/addItem');
+const updateItem = require('./item/updateItem');
+const deleteItem = require('./item/deleteItem');
 
 router.get('/items', getItems);
 router.post('/items', addItem);
