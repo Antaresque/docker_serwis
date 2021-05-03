021119066c92ecc754241d4b43dc7330b9b71c54
RzulwWojownik <adijedendwa@gmail.com>
2021-05-03 18:20:47 +0200

    sqlv1

--- frontend/main.js
+++ frontend/main.js
@@ -1,4 +1,4 @@
-const API_URI = "http://data:3000";
+const API_URI = "http://localhost:3000";
 
 async function getData(){
     try {
@@ -13,7 +13,7 @@ async function getData(){
         console.error(err);
     }
 }
-
+//papiez polak gwalcil male dzieci
 document.getElementById("test").addEventListener("click", async (ev) => {
     let data = await getData();
     document.getElementById("dane").innerHTML = JSON.stringify(data);
