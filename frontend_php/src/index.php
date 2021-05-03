e057c64702a26c6143c3222f0922a7109aa91f9e
Antaresque <olekfer@yahoo.pl>
2021-05-03 19:49:57 +0200

    PHP

---
+++ frontend_php/src/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    <title>Website</title>
+</head>
+<body>
+    <h1>Hello World!</h1>
+    <button id="test">TEST</button>
+    <?php
+        echo "test3"
+    ?>
+    <div id="dane">DANE</div>
+    <script src="main.js"></script>
+</body>
+</html>
