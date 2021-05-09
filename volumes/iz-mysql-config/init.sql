3beb745940627712e1ade63fb59a926f9a115919
RzulwWojownik <adijedendwa@gmail.com>
2021-05-09 22:17:48 +0200

    testowe dane

--- volumes/iz-mysql-config/init.sql
+++ volumes/iz-mysql-config/init.sql
@@ -40,6 +40,16 @@ CREATE TABLE `comments` (
   `crdate` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_polish_ci;
 
+--
+-- Zrzut danych tabeli `comments`
+--
+
+INSERT INTO `comments` (`id`, `userid`, `comment`, `date`, `upvotes`, `downvotes`, `imgid`, `crdate`) VALUES
+(1, 4, 'noooo powiem ci uczniu pjenkne pjenkne, widac ten skill w fotoszopie', '2021-05-02', 2137, 69, 4, '2021-05-02 21:37:37'),
+(2, 2, 'chujowe nie pozdrawiam walaszek lepszy styl ma', '2021-05-03', 69, 420, 4, '2021-05-03 00:00:00'),
+(3, 5, 'obrazek piekny ale uzytkownik gnoj', '2021-05-10', 2137, 1, 3, '2021-05-10 00:00:00'),
+(4, 5, 'kurla ale taka to by do domu zabral MMMM', '2021-05-05', 69, 3, 7, '2021-05-05 21:37:00');
+
 -- --------------------------------------------------------
 
 --
@@ -54,18 +64,21 @@ CREATE TABLE `images` (
   `comments` int(11) NOT NULL,
   `title` varchar(50) COLLATE utf8mb4_polish_ci NOT NULL,
   `description` text COLLATE utf8mb4_polish_ci NOT NULL,
-  `crdate` datetime NOT NULL,
-  `createdAt` date DEFAULT NULL,
-  `updatedAt` date DEFAULT NULL
+  `crdate` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_polish_ci;
 
 --
 -- Zrzut danych tabeli `images`
 --
 
-INSERT INTO `images` (`id`, `userid`, `address`, `upvotes`, `comments`, `title`, `description`, `crdate`, `createdAt`, `updatedAt`) VALUES
-(1, 1, '1.jpg', 20, 2, 'test', 'test', '2021-05-05 20:18:40', NULL, NULL),
-(2, 1, '2.jpg', 30, 3, 'nazwa', 'dpgjdsopgjsdpogjdsopgjsopg', '2021-05-05 20:19:02', NULL, NULL);
+INSERT INTO `images` (`id`, `userid`, `address`, `upvotes`, `comments`, `title`, `description`, `crdate`) VALUES
+(1, 1, '1.jpg', 20, 2, 'test', 'test', '2021-05-05 20:18:40'),
+(2, 1, '2.jpg', 30, 3, 'nazwa', 'dpgjdsopgjsdpogjdsopgjsopg', '2021-05-05 20:19:02'),
+(3, 2, 'papiez.png', 69, 1, 'hehe pawulon', '2137', '2021-05-09 00:00:00'),
+(4, 5, 'reimu.png', 21, 37, 'reimu na rzulfiu auuuuu', 'zajelo 7h co sadzicie', '2021-05-02 21:37:00'),
+(5, 5, 'lampa.png', 69, 420, 'pjenkna lampa z umineko', 'oto lampa z tieru B mojej top tier listy lamp z umineko', '2021-05-02 21:37:00'),
+(6, 4, 'beatoreechee.png', 11037, 21, 'co tam batora', 'znowu nawiedzmowane', '2021-05-02 21:37:00'),
+(7, 4, 'rika.png', 10000, 22, 'nipaaaa', 'nipa cute', '2021-05-02 21:37:00');
 
 -- --------------------------------------------------------
 
@@ -86,7 +99,11 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `nickname`, `pass`, `email`, `crdate`) VALUES
-(1, 'janusz', 'janusz123', 'janusz@test.pl', '2021-05-05 20:18:12');
+(1, 'janusz', 'janusz123', 'janusz@test.pl', '2021-05-05 20:18:12'),
+(2, 'seba2137', '2137', 'sebastian@seba.com', '2021-05-08 11:19:39'),
+(3, 'pawulon69', 'jp2gmd', 'papaj@interia.pl', '2021-05-03 10:14:38'),
+(4, 'antaresque-', 'altajtopedal', 'antaresque@gmail.com', '2021-05-01 21:37:00'),
+(5, 'Rolsig', 'heheciota', 'dante.rydzyk@gmail.com', '2021-05-01 21:37:00');
 
 --
 -- Indeksy dla zrzutów tabel
@@ -115,26 +132,26 @@ ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT dla zrzuconych tabel
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
 -- AUTO_INCREMENT dla tabeli `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT dla tabeli `images`
 --
 ALTER TABLE `images`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT dla tabeli `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Ograniczenia dla zrzutów tabel
@@ -157,4 +174,3 @@ COMMIT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
