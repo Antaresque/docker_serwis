dda7e6a8c87a7e8fa9569702246d09e613a1bbdb
RzulwWojownik <adijedendwa@gmail.com>
2021-05-25 21:57:41 +0200

    cummit

--- frontend_php/src/register.php
+++ frontend_php/src/register.php
@@ -1,6 +1,18 @@
 <?php
-    echo $_POST["username"];
-    echo $_POST["email"];
-    echo $_POST["password"];
-    echo $_POST["passwordR"];
+    require_once 'curl_helper.php';
+    
+    $sus = $_POST["username"];
+    $pus = $_POST["password"];
+    $pus2 = $_POST["passwordR"];
+    $emus = $_POST["email"];
+
+    if($pus == $pus2){
+        $arr = array("user" => $sus, 
+                    "pass" => $pus,
+                    "email" => $emus);
+        echo http_build_query($arr) . "\n";
+        $data = CurlHelper::perform_http_request("POST", "http://api:4000/login/", $arr);
+        echo $data;
+    }
+    
 ?>
