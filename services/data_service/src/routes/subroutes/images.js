916bb7548346b8390961ae6749825b352828d381
Antaresque <olekfer@yahoo.pl>
2021-05-27 00:33:30 +0200

    data service ścieżki /images

--- services/data_service/src/routes/subroutes/images.js
+++ services/data_service/src/routes/subroutes/images.js
@@ -1,3 +1,4 @@
+const { response } = require("express");
 const models = require("../../models");
 const Image = models.Image;
 const ImageView = models.ImageView;
@@ -37,15 +38,83 @@ async function getImages(req, res){
 }
 
 async function addImage(req, res){
-    return res.sendStatus(404);
+    const { userid, title, description, address } = req.body;
+
+    if(userid && title && description && address){
+        const newImage = {
+            userid: userid,
+            title: title,
+            description: description,
+            address: address
+        }
+
+        try {
+            const record = await Image.create(newImage);
+            res.send(record);
+        }
+        catch(err){
+            console.log(err.message);
+            res.sendStatus(500);
+        }
+    
+        
+    }
+    else return res.sendStatus(400);
 }
 
 async function changeImage(req, res){
-    return res.sendStatus(404);
+    const { id } = req.params;
+    if(!id || id === undefined)
+        return res.sendStatus(400);
+
+    const { title, description } = req.body;
+    
+    let obj = {};
+    if(title)
+        obj.title = title;
+    if(description)
+        obj.description = description;
+
+    if(obj === {})
+        return res.sendStatus(400);
+
+    try {
+        const records = await Image.update(obj, { where: { id: id } });
+        if(!records)
+            throw("No updated image");
+        
+        const updatedImage = await Image.findByPk(id);
+        if(!updatedImage)
+            throw("No updated image");
+
+        res.send(updatedImage);
+    }
+    catch(err){
+        console.log(err.message);
+        res.sendStatus(500); 
+    }
 }
 
 async function deleteImage(req, res){
-    return res.sendStatus(404);
+    const { id } = req.params;
+    if(!id)
+        return res.sendStatus(400);
+
+    try {
+        const destroyedImage = await Image.findByPk(id);
+        if(!destroyedImage)
+            return res.status(400).send("No image with this ID");
+
+        const records = await Image.destroy({ where: { id: id } });
+        if(!records)
+            throw("No records removed");
+
+        res.send(destroyedImage);
+    }
+    catch(err) {
+        console.log(err.message);
+        res.sendStatus(500);
+    }
 }
 
 module.exports = {
