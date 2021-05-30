815f2f3c63b1f8683f013c46e885d4b23d08c066
Antaresque <olekfer@yahoo.pl>
2021-05-30 15:27:24 +0200

    API vote implementation

--- api/src/routes/user/votes.js
+++ api/src/routes/user/votes.js
@@ -1,17 +1,93 @@
 const Vote = require("../../controllers/data_vote");
 
 async function setImageVote(req, res){
-    return res.sendStatus(404);
+    const { id: token_user } = req.payload;
+    const { id: imgid } = req.params;
+
+    if(!token_user)
+        return res.status(401);
+
+    if(!imgid)
+        return res.status(400).send("Brak id");
+
+    try {
+        const data = await Vote.addForImage(imgid, token_user);
+
+        return res.send(data);
+    }
+    catch(err){
+        console.log(err.message);
+        if(res.status)
+            return res.status(err.status).send(err.message);
+        else return res.sendStatus(500);
+    }
 }
 async function undoImageVote(req, res){
-    return res.sendStatus(404);
+    const { id: token_user } = req.payload;
+    const { id: imgid } = req.params;
+
+    if(!token_user)
+        return res.status(401);
+
+    if(!imgid)
+        return res.status(400).send("Brak id");
+
+    try {
+        const data = await Vote.removeForImage(imgid, token_user);
+
+        return res.send(data);
+    }
+    catch(err){
+        console.log(err.message);
+        if(res.status)
+            return res.status(err.status).send(err.message);
+        else return res.sendStatus(500);
+    }
 }
 
 async function setCommentVote(req, res){
-    return res.sendStatus(404);
+    const { id: token_user } = req.payload;
+    const { id: cid } = req.params;
+
+    if(!token_user)
+        return res.status(401);
+
+    if(!cid)
+        return res.status(400).send("Brak id");
+
+    try {
+        const data = await Vote.addForComment(cid, token_user);
+
+        return res.send(data);
+    }
+    catch(err){
+        console.log(err.message);
+        if(res.status)
+            return res.status(err.status).send(err.message);
+        else return res.sendStatus(500);
+    }
 }
 async function undoCommentVote(req, res){
-    return res.sendStatus(404);
+    const { id: token_user } = req.payload;
+    const { id: cid } = req.params;
+
+    if(!token_user)
+        return res.status(401);
+
+    if(!cid)
+        return res.status(400).send("Brak id");
+
+    try {
+        const data = await Vote.removeForComment(cid, token_user);
+
+        return res.send(data);
+    }
+    catch(err){
+        console.log(err.message);
+        if(res.status)
+            return res.status(err.status).send(err.message);
+        else return res.sendStatus(500);
+    }
 }
 
 
